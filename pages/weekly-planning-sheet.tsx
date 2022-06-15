3bdf0dd56b4a172821979fc9bedf731d5b172c16
jdcampolargo <jdcampolargo@hotmail.com>
2022-06-14 22:32:01 -0500

    Pushing Updates

--- pages/weekly-planning-sheet.tsx
+++ pages/weekly-planning-sheet.tsx
@@ -10,7 +10,7 @@ Many people are unproductive because they spend thinking about what they’re go
 
 This also helps with procrastination. </p>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/9v_kL6YVaAg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9v_kL6YVaAg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 <p>&nbsp;  </p>
 <p>&nbsp;  </p>
