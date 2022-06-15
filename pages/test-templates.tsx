7b32d91b2a51099b85540d1ae91ce12a5ce6c7a8
jdcampolargo <jdcampolargo@hotmail.com>
2022-06-14 22:40:06 -0500

    Pushing Updates

--- pages/test-templates.tsx
+++ pages/test-templates.tsx
@@ -1,10 +1,10 @@
 function Test_Templates() {
     return <div>
         <h2>How to take Tests</h2>
-        <p> Taking tests is a skill. Here are some thoughts on how to take tests. <a href="https://www.juandavidcampolargo.com/how-to-take-tests" target= "_blank" >Learn more.</a>  </p>
+        <p> Taking tests is a skill. Here are some thoughts on how to take tests. <a href="https://www.juandavidcampolargo.com/how-to-take-tests">Learn more.</a>  </p>
 
         <h2>The Test Checklist</h2>
-        <p> Change the framing: Tests are opportunities to learn. Go through it before every test. <a href="https://www.juandavidcampolargo.com/test-checklist" target = "_blank" >Learn more.</a>  </p>
+        <p> Change the framing: Tests are opportunities to learn. Go through it before every test. <a href="https://www.juandavidcampolargo.com/test-checklist">Learn more.</a>  </p>
     </div>
 
   }
