509130609ca3ba42739f2afc11da99c71f2fb485
jdcampolargo <jdcampolargo@hotmail.com>
2022-06-20 14:13:54 -0500

    Pushing Updates

--- pages/amazonemailtemplate.tsx
+++ pages/amazonemailtemplate.tsx
@@ -1,16 +1,26 @@
-function AmazonEmailTemplate() {
-  return (
-    <div>
-      <h1>Amazon Email Template</h1>
+// function AmazonEmailTemplate() {
+//   return (
+//     <div>
+//       <h1>Amazon Email Template</h1>
 
 
-      <img src="/images/az1.jpg" alt="Girl in a jacket" width={700} height={1234}/>
+//       <img src="/images/az1.jpg" alt="Girl in a jacket" width={700} height={1234}/>
 
-      <p> Amazom Email Template</p>
+//       <p> Amazom Email Template</p>
 
-      </div>
+//       </div>
 
-    );
-}
+//     );
+// }
 
-export default AmazonEmailTemplate;
+// export default AmazonEmailTemplate;
+
+function Contact() {
+    return <div> Send me
+    
+    <a href="mailto:jdcampolargo@hotmail.com"> an email</a>
+
+    </div>
+  }
+  
+  export default Contact
---
+++ pages/test.js
@@ -0,0 +1,3 @@
+// take JSON data and send it to endpoint
+// get JSON data from endpoint
+// display JSON data on page
