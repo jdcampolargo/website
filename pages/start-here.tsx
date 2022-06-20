20fd8a836b521a8b3a7f2cb634f61b2c5db39550
jdcampolargo <jdcampolargo@hotmail.com>
2022-06-20 15:13:04 -0500

    ok here we go

---
+++ pages/start-here.tsx
@@ -0,0 +1,5 @@
+// redirect to /about page using Link component
+import Link from 'next/link';
+
+//when people go to /start-here, they will automatically redirected to /about. Automatically redirect
+//to /about page when they go to /start-here
