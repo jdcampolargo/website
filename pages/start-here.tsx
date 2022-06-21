4a57340285d391b5a0dc41e4aedb3cb392284541
jdcampolargo <jdcampolargo@hotmail.com>
2022-06-21 13:16:06 -0500

    Pushing Updates

--- pages/start-here.tsx
+++ pages/start-here.tsx
@@ -3,3 +3,12 @@ import Link from 'next/link';
 
 //when people go to /start-here, they will automatically redirected to /about. Automatically redirect
 //to /about page when they go to /start-here
+function Contact() {
+    return <div> Send me
+    
+    <a href="mailto:jdcampolargo@hotmail.com"> an email</a>
+
+    </div>
+  }
+  
+  export default Contact
