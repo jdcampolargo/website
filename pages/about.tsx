d7a36160ef0382faee78359d693f29fb2e5ad856
jdcampolargo <jdcampolargo@hotmail.com>
2022-06-28 19:15:32 -0500

    Pushing Updates

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,54 @@
-function Contact() {
-    return <div> Send me
-    
-    <a href="mailto:jdcampolargo@hotmail.com"> an email</a>
 
+
+  // write short bio about me with the following information:
+//   7 - Second Bio
+// Born in Venezuela
+// Moved to Chicago when I was 13 
+// Wrote a book (Generation Optimism)
+// Biophysics Researcher (Antimicrobial Peptides)
+// Engineering Student at the University of Illinois at Urbana-Champaign
+//
+// Language: typescript
+// Path: next-blog/pages/about.tsx
+function Bio() {
+    return <div>
+    <p>
+    My name is Juan David Campolargo.
+    </p>
+    <p>
+      This is the story of my life, who I am, where I come from, and what I'm about. 
+    </p>
+    <p>
+    I was born and raised in Venezuela. 
+    </p>
+    <p>
+    Biophysics Researcher (Antimicrobial Peptides)
+    </p>
+    <p>
+    Engineering Student at the University of Illinois at Urbana-Champaign
+    </p>
     </div>
   }
-  
-  export default Contact
+
+
+export default Bio
+
+
+
+// I don't really want to do the bio to be honest with you. It will be long and will take time. The Start-here too. I'm having trouble trying to decide how to structure this and actually pushed it.
+
+// Perhaps I should try the press release thing that tony fadell or jeff bezos thing. 
+
+// Let's do it right now.
+
+// Press Release
+// Juan David Campolargo, the young inventor, writer, engineer, author, and really renaissance known for his advances in drone and personal flying machine technology as well as biology and his intellectual pursuits and his passion for the sciences has released a brand new website he himself coded!
+// This website is impressive, new, and easy to read. It also serves as a collection of his essays throughout the year as well as who he is and what he's about.
+// Some features include
+// The color and font and style are really neat and easy to read and reminded of open AI
+// I like how Juan David's coded his own website and really tries to not show off at all and put the essays as the first thing on there. 
+// The colors of his website are interesting and are reflection of his venezuelan colors, his personality, and curious mind. 
+// The couple other thing on there are cool too like the bio and the links to the other pages. The bio is probably one of the biggest things because I could really understand who the person was, his struggles, and how similar I am to him. 
+
+// simple webpage and nice to read. 
+
