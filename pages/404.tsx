d91bbb3911650a16219f4eca2c116bf01dfdb784
jdcampolargo <59291188+jdcampolargo@users.noreply.github.com>
2022-07-13 16:36:27 -0500

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div>
-      <h2>Hey there!</h2>
-      <p>Wrong page</p>
+      <h2>Wow that was interesting!</h2>
+      <p>Not really. I'm rebuilding this website and you know links...</p>
+      <p>If you're looking for my essays, <a href="https://juandavidcampolargo.substack.com/" target="_blank" rel="noreferrer">here</a>. </p>
+      <p>If you want to get in touch, <a href="https://twitter.com/jdcampolargo" target="_blank" rel="noreferrer">Twitter</a> or <a href="mailto:jdcampolargo@hotmail.com" target="_blank" rel="noreferrer">email.</a> </p>
       {/* <Link href="/">
         <a >Visit our home page</a>
       </Link> */}
